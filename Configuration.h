e72b5550bfc9528e546d93dc557481407aae067d
Vincent Rocher <cybervinc@gmail.com>
2015-04-23 16:09:05 +0200

    probe offset change
    again

--- Configuration.h
+++ Configuration.h
@@ -425,9 +425,9 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
   // these are the offsets to the probe relative to the extruder tip (Hotend - Probe)
   // X and Y offsets must be integers
 
-  #define X_PROBE_OFFSET_FROM_EXTRUDER 21
-  #define Y_PROBE_OFFSET_FROM_EXTRUDER -8
-  #define Z_PROBE_OFFSET_FROM_EXTRUDER -25.4
+  #define X_PROBE_OFFSET_FROM_EXTRUDER 23
+  #define Y_PROBE_OFFSET_FROM_EXTRUDER -3
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER -7.3
 
 
   #define Z_RAISE_BEFORE_HOMING 40       // (in mm) Raise Z before homing (G28) for Probe Clearance.
